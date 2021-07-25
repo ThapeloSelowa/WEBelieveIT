49bc79a974472445a3c6f44533d0f85940dee7bf
WIN10PRO <WIN10PRO@DESKTOP-Q7O09VI>
2021-07-25 15:17:41 +0200

    Task Status Created

---
+++ Models/Enums/TaskStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ModelLibrary.Enums
+{
+    enum TaskStatus
+    {
+        [Description("Not In Progress")]
+        NotInProgress,
+        [Description("In Progress")]
+        InProgress,
+        [Description("Complete")]
+        Complete,
+        [Description("Partially Complete")]
+        PartiallyComplete,
+        [Description("Cancelled")]
+        Cancelled,
+        [Description("Deleted")]
+        Deleted
+    }
+}
